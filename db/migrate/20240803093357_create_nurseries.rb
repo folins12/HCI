523b0de80d50cce63330a7e79b2bde93f7dfcf27
Federico Angarella <federico.angarella@gmail.com>
2024-08-03 12:01:24 +0200

    nursery
    aumentato il database dei vivai

--- db/migrate/20240803093357_create_nurseries.rb
+++ db/migrate/20240803093357_create_nurseries.rb
@@ -2,9 +2,15 @@ class CreateNurseries < ActiveRecord::Migration[6.1]
   def change
     create_table :nurseries do |t|
       t.string :name
+      t.integer :number
+      t.string :email
+      t.string :address
       t.string :location
+      t.integer :open_time
+      t.integer :close_time
       t.text :description
 
+      t.timestamps
     end
   end
 end
