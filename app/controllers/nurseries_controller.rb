1a1d8bf880724b15758096b9664bd1851d3a1601
folins12 <afolix2003@gmail.com>
2024-08-04 19:48:15 +0200

    .

--- app/controllers/nurseries_controller.rb
+++ app/controllers/nurseries_controller.rb
@@ -1,11 +1,10 @@
 class NurseriesController < ApplicationController
-    
   def index
-    @nurseries = params[:query].present? ? Nursery.search(params[:query]) : Nursery.all
+    @nurseries = params[:query].present? ? Nursery.search(params[:query]) : Nursery.order(:position)
+    @max_id = Nursery.maximum(:id) #|| 6  Imposta l'ID massimo presente nel database o usa 6 come valore predefinito
   end
 
   def show
     @nursery = Nursery.find(params[:id])
   end
-
 end
