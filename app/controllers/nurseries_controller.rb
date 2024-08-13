99108bf3fa7c5e0df41542f3002ebcbed9d7a7d4
Federico Angarella <federico.angarella@gmail.com>
2024-08-13 18:36:08 +0200

    bug fixing

--- app/controllers/nurseries_controller.rb
+++ app/controllers/nurseries_controller.rb
@@ -1,4 +1,15 @@
 class NurseriesController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @nurseries = params[:query].present? ? Nursery.search(params[:query]) : Nursery.order(:position)
+    @max_id = Nursery.maximum(:id)
+  end
+
+  def show
+    @nursery = Nursery.find(params[:id])
+    @plants = @nursery.plants
+  end
+  
   def new
     @nursery = Nursery.new
   end
