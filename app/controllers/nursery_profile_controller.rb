e0867f3285238b1df733d47b01bfb07d2ac7182c
michele461 <libriani.1954541@studenti.uniroma1.it>
2024-08-15 11:21:07 +0200

    .

--- app/controllers/nursery_profile_controller.rb
+++ app/controllers/nursery_profile_controller.rb
@@ -3,35 +3,37 @@ class NurseryProfileController < ApplicationController
   before_action :set_nursery, only: [:profile, :update_profile]
 
   def profile
-    @nursery = Nursery.find_by(id_owner: current_user.id)
-    nursery_ids = Nursery.where(id_owner: current_user.id).pluck(:id)
-    nursery_plant_ids = NurseryPlant.where(nursery_id: nursery_ids).pluck(:id)
-  
-    @myplants = Plant.joins(:nursery_plants)
-                     .where(nursery_plants: { id: nursery_plant_ids })
-                     .select('plants.id, plants.name, nursery_plants.id as nursery_plant_id, nursery_plants.max_disponibility as disp, nursery_plants.num_reservations as res,
-                              typology, light, size, irrigation, use, climate, description')
-  
-    @reservations = Reservation.joins(:nursery_plant)
-                               .where(nursery_plants: { id: nursery_plant_ids })
-                               .pluck(:user_email, :nursery_plant_id)
-                               .group_by { |email, id| id }
+    load_nursery_data
   end
 
   def update_profile
-    # Aggiorna il profilo solo se i parametri sono presenti
-    if @nursery.update(nursery_params)
-      flash[:notice] = "Profilo aggiornato con successo."
-      redirect_to nursery_profile_path
-    else
-      # Ricarica le piante e le prenotazioni se c'è un errore
-      load_nursery_data
-      flash.now[:alert] = "Errore nell'aggiornamento del profilo."
-      render :profile
+    if params[:nursery_user]
+      if profile_update_valid?
+        @user.update(user_params)
+      else
+        load_nursery_data
+        flash.now[:alert] = "Errore nell'aggiornamento del profilo. Verifica i dati inseriti."
+        render :profile and return
+      end
+    end
+  
+    if params[:nursery]
+      if nursery_update_valid?
+        @nursery.update(nursery_params)
+        flash[:notice] = "Profilo aggiornato con successo."
+        redirect_to nursery_profile_path
+      else
+        load_nursery_data
+        flash.now[:alert] = "Errore nell'aggiornamento del vivaio. Verifica i dati inseriti."
+        render :profile
+      end
     end
   end
+  
+  
 
   def edit_profile
+    # Render the edit profile view
   end
 
   private
@@ -59,15 +61,13 @@ class NurseryProfileController < ApplicationController
     @nursery = Nursery.find_by(id_owner: current_user.id)
   end
 
-  def user_params
-    params.require(:nursery_user).permit(:nome, :cognome, :current_password, :password, :password_confirmation, :address)
-  end
-
   def nursery_params
     params.require(:nursery).permit(:name, :address, :number, :email, :open_time, :close_time, :description)
   end
 
   def profile_update_valid?
+    return true unless params[:nursery_user] # Nessun controllo necessario se non ci sono modifiche al profilo
+  
     if params[:nursery_user][:password].blank? && params[:nursery_user][:password_confirmation].blank?
       if params[:nursery_user][:current_password].present?
         unless @user.authenticate(params[:nursery_user][:current_password])
@@ -77,37 +77,59 @@ class NurseryProfileController < ApplicationController
       end
       return true
     end
-
+  
     if params[:nursery_user][:current_password].blank?
       @user.errors.add(:current_password, 'La password attuale è richiesta.')
       return false
     end
-
+  
     unless @user.authenticate(params[:nursery_user][:current_password])
       @user.errors.add(:current_password, 'Password errata!')
       return false
     end
-
+  
     if params[:nursery_user][:password] == params[:nursery_user][:current_password]
       @user.errors.add(:password, 'La nuova password non può essere uguale alla precedente')
       return false
     end
-
+  
     unless valid_password?(params[:nursery_user][:password])
       @user.errors.add(:password, 'La nuova password non rispetta i requisiti: - almeno una maiuscola; - almeno una minuscola; - almeno un numero; - almeno un carattere speciale.')
       return false
     end
-
+  
     unless params[:nursery_user][:password] == params[:nursery_user][:password_confirmation]
       @user.errors.add(:password_confirmation, 'La password confermata deve essere uguale a quella nuova precedentemente inserita')
       return false
+    end
+  
+    true
+  end
+  
+
+  def nursery_update_valid?
+
+    number = params[:nursery][:number].to_s.strip
+    unless number.length == 10 && number.match?(/\A\d{10}\z/)
+      @nursery.errors.add(:number, 'Il numero di telefono deve essere composto esattamente da 10 cifre.')
+      return false
+
+    if @nursery.open_time >= @nursery.close_time
+      @nursery.errors.add(:base, 'La fascia oraria inserita non è valida.')
+      return false
+    end
+
+    if @nursery.description.blank?
+      @nursery.errors.add(:description, 'La descrizione non può essere vuota.')
+      return false
+    end
+
     end
 
     true
   end
 
   def valid_password?(password)
-    # Definisci qui i criteri di validità della password
     password.length >= 8 && password.match(/[A-Z]/) && password.match(/[a-z]/) && password.match(/[0-9]/) && password.match(/[\W_]/)
   end
 end
