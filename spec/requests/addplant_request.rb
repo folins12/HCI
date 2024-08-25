15c4ae935b4ed068300806cd93ba3498e3bb22ce
Federico Angarella <federico.angarella@gmail.com>
2024-08-25 17:15:04 +0200

    sempre test

--- spec/requests/addplant_request.rb
+++ spec/requests/addplant_request.rb
@@ -9,36 +9,35 @@ RSpec.describe "Myplants", type: :request do
                               irrigation: '1', size: '1', climate: 'Arido', use: 'Medicinale') }
   let!(:myplant) { Myplant.create(user: user, plant: plant) }
 
-  
-
   before do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
-  describe "POST /addmyplant" do
 
+  describe "POST /addmyplant" do
     context "when the request is valid" do
       it "creates a new Myplant and returns success" do
         post '/addmyplant', params: { plant_id: plant.id }, headers: { 'ACCEPT' => 'application/json' }
         expect(response).to have_http_status(:ok)
         json_response = JSON.parse(response.body)
+
         expect(json_response['success']).to be_truthy
       end
+      
+      context "when the request is invalid" do
+        it "returns an error when saving fails" do
+          allow_any_instance_of(Myplant).to receive(:save).and_return(false)
+          allow_any_instance_of(Myplant).to receive_message_chain(:errors, :full_messages).and_return(["Errore nel salvataggio della prenotazione."])
+          post '/addmyplant', params: { plant_id: nil }, headers: { 'ACCEPT' => 'application/json' }
+          
+          expect(response).to have_http_status(:unprocessable_entity)
+          json_response = JSON.parse(response.body)
+          expect(json_response['success']).to be_falsey
+          expect(json_response['message']).to eq("Errore nel salvataggio della prenotazione.")
+          expect(json_response['errors']).to include("Errore nel salvataggio della prenotazione.")
+        end
+      end
+
     end
-
-    #context "when the request is invalid" do
-    #  it "returns an error when saving fails" do
-    #    # Simula un errore di salvataggio
-    #    allow_any_instance_of(Myplant).to receive(:save).and_return(false)
-    #    allow_any_instance_of(Myplant).to receive_message_chain(:errors, :full_messages).and_return(["Errore nel salvataggio della prenotazione."])
-    #    post '/addmyplant', params: { plant_id: nil }, headers: { 'ACCEPT' => 'application/json' }
-    #    expect(response).to have_http_status(:unprocessable_entity)
-    #    json_response = JSON.parse(response.body)
-    #    expect(json_response['success']).to be_falsey
-    #    expect(json_response['message']).to eq("Errore nel salvataggio della prenotazione.")
-    #    expect(json_response['errors']).to include("Errore nel salvataggio della prenotazione.")
-    #  end
-    #end
-
   end
 
   describe "POST /removemyplant" do
@@ -54,16 +53,17 @@ RSpec.describe "Myplants", type: :request do
       end
     end
 
-    #context "when the myplant does not exist" do
-    #  it "returns an error" do
-    #    # Proviamo a rimuovere una Myplant che non esiste
-    #    delete '/removemyplant', params: { plant_id: 999 }, headers: { 'ACCEPT' => 'application/json' } # Un ID che non esiste
-    #    expect(response).to have_http_status(:unprocessable_entity)
-    #    json_response = JSON.parse(response.body)
-    #    expect(json_response['success']).to be_falsey
-    #    expect(json_response['message']).to eq("Errore nella rimozione della pianta.")
-    #    expect(json_response['errors']).to include("Pianta non trovata")
-    #  end
-    #end
+    context "when the myplant does not exist" do
+      it "returns an error" do
+        # Proviamo a rimuovere una Myplant che non esiste
+        post '/removemyplant', params: { plant_id: 999 }, headers: { 'ACCEPT' => 'application/json' } # Un ID che non esiste
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response['success']).to be_falsey
+        expect(json_response['message']).to eq("Errore nella rimozione della pianta.")
+        expect(json_response['errors']).to include("Pianta non trovata")
+      end
+    end
+
   end
 end
